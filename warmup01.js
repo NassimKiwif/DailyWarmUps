97b26ff0f686136085125e06851d73321b5385f5
nassimkiwif <nassim_alias@hotmail.fr>
2024-01-12 10:48:49 +0100

    warmup80%

--- warmup01.js
+++ warmup01.js
@@ -1,26 +1,27 @@
 // 1-using + operator create a function that combine your partner first and last name and return it.*//
 
 // function declaration
-function MyPartnerName(firstname, lastname){
-// write yourcode 
-   }
+function MyPartnerName(firstname, lastname) {
+	// write yourcode 
+}
 // function expression 
 
-let myPartnerName=function(firstname,lastname){
-//write your code
+let myPartnerName = function (firstname, lastname) {
+	//write your code
 }
 
 // arrow function 
 
-let PartnerName2 = (firstName,lastName) => {
-// write your code 
+let PartnerName2 = (firstName, lastName) => {
+	// write your code 
 }
 
 // 2-create a function that return true if the number 13 is a multiple of 3 and false if not.*//
 
-function MultipleOf13orNot(a){
+function MultipleOf13orNot(a) {
 	let result;
-	if (13%3)
+	if (13 % 3) { return false }
+	else { return true }
 }
 // function expression 
 
@@ -30,11 +31,11 @@ function MultipleOf13orNot(a){
 
 // 4-create a function that takes a age as a parametre and return that age in seconds.*//
 
-function AgeInSeconds(age){
+function AgeInSeconds(age) {
 	//write your code
 }
 
-// function expression 
+// function expression
 
 
 
--- warmup2.js
+++ warmup2.js
@@ -2,16 +2,32 @@
  * 1 - Complete the function cube that returns the cube of x:
  */
 function cube(x) {
+    return x ** 3
 }
 /**
  * 2 - Write a function sameLength that accepts two strings as arguments, and returns true if those strings have the same length, and false otherwise.
  */
-function sameLength(string1, string2) {      
-}
+function sameLength(str1, str2) {
+    return str1.length === str2.length;
+  }
+  
 /**
  * 3 - Write a function called scoreToGrade that accepts a number as a parameter and returns a string representing a letter grade corresponding to that score.
  */
-function scoreToGrade(score){
+function scoreToGrade(score) {
+    function scoreToGrade(score) {
+        if (score >= 90) {
+          return "A";
+        } else if (score >= 80) {
+          return "B";
+        } else if (score >= 70) {
+          return "C";
+        } else if (score >= 60) {
+          return "D";
+        } else {
+          return "F";
+        }
+      }      
 }
 /**
  * Repeating a String n Times: Let's write a function called repeatString that takes two parameters: a string str, which is the string to be repeated, and count -- a number representing how many times the string str should be repeated
@@ -20,6 +36,3 @@ function scoreToGrade(score){
  * repeatString('dog', 2); // => 'dog' + 'dog' => 'dogdog'
  * repeatString('dog', 3); // => 'dog' + 'dog' + 'dog' => 'dogdogdog'
  */
-function repeatString(string, num) {
-  
-} 
