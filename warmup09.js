2f5a1727cebc6b3bdc0657681a0c1f22a01ae59e
nassimkiwif <nassim_alias@hotmail.fr>
2024-01-23 11:31:28 +0100

    Wip

--- warmup09.js
+++ warmup09.js
@@ -8,23 +8,31 @@
 // - calculate the number of male friends that your class have by writing a function called nbOfMale.
 // - Write a function searchMates that, given a query and an array of Mates,
 //   searches the array of mates for "matching" mate. You will decide what way you want to write your search algorithm.
-function createClassmates(name,family_name,age,phase,points){
-    return{
-        name:name,
+function createClassmates(name, family_name, age, phase, points) {
+    return {
+        name: name,
         family_name,
         age,
         phase,
         points
     }
 }
-let ahmed=createClassmates("ahmed","ghalleb",26,"basic pack",7)
-let sabrine=createClassmates("sabrine","zouaoi",27,"fronttower pack",-24)
-let nassim=createClassmates("nassim","kefi",25,"baackbridge pack",1.5)
-let family=[ahmed,sabrine,nassim]
+let ahmed = createClassmates("ahmed", "ghalleb", 26, "basic pack", 7)
+let sabrine = createClassmates("sabrine", "zouaoi", 27, "fronttower pack", -24)
+let nassim = createClassmates("nassim", "kefi", 25, "baackbridge pack", 1.5)
+let family = [ahmed, sabrine, nassim]
 
-function displayFriend(classmate){
-    return "The classmate name is "+classmate.name+classmate.family_name+" he's "+classmate.age+" Years old "+"he's actually studying the "+classmate.phase+" and he has "+classmate.points+" points"
+function displayFriend(classmate) {
+    return "The classmate name is " + classmate.name + " " + classmate.family_name + " he's " + classmate.age + " Years old " + "he's actually studying the " + classmate.phase + " and he has " + classmate.points + " points"
 }
-function addFriend(Newclassmate){
-    
+//or 
+// return `name is ${classmate.name}...`
+function addFriend(Newclassmate) {
+    family.push(Newclassmate)
+    return family
 }
+function nbOfMale(array){
+    let counter=0
+    for(let)
+}e
+
