3aa3148a12df301ce08d3bc821c4ebc248f4332a
nassimkiwif <nassim_alias@hotmail.fr>
2024-01-26 09:32:13 +0100

    0

--- warmup12.js
+++ warmup12.js
@@ -1,16 +1,3 @@
- // Write a JavaScript function that will iterate from n to 0. For each iteration,
- // it will check if the current number is odd or even, and display a message to the screen.
- // iterateAndLog(3); =>
- // Sample Output :
- // "2 is even"
- // "1 is odd"
- // "0 is even"
-
-function iterateAndLog(n) {
-
-  // TODO: your work goes here
-
-}
 
  // Write a JavaScript function that will iterate from n to 0. For each iteration,
  // it will check if the current number is odd or even, and display a message to the screen.
